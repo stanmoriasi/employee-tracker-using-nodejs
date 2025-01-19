28a59b0441b589f30e31bce59ac256e0fbd7bbbf
stanmoriasi <stanmoriasi@yahoo.com>
2025-01-18 17:49:42 -0700

    update id to be optional in employee interface

--- src/departments.ts
+++ src/departments.ts
@@ -44,6 +44,8 @@ class Departments{
 });
     }
 
+    
+
 }
 
 
--- src/employees.ts
+++ src/employees.ts
@@ -2,7 +2,7 @@ import { QueryResult } from "pg";
 import {pool} from "./connection.js";
 
 interface employee {
-    id: number;
+    id?: number;
     firstName: string;
     lastName: string;
     roleId: number;
@@ -16,21 +16,40 @@ class employees {
         this.employees.push(employee);
         //const query = `INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES (${employee.id}, '${employee.firstName}', '${employee.lastName}', ${employee.roleId}, ${employee.managerId})`;
         //const values = [employee.id, employee.firstName, employee.lastName, employee.roleId, employee.managerId];
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`;
+        const values = [employee.firstName, employee.lastName, employee.roleId, employee.managerId];
+        return new Promise((resolve, reject) => {
+            pool.query(sql, values, (err: Error, result: QueryResult) => {
+                if (err) {
+                    console.error('Error executing query', err.stack);
+                    reject(err);
+                    return;
+                }
+                console.log('Employee added successfully');
+                    resolve(result);
+                });
+            });
         }
-
-    public getEmployees() {
-        const sql = 'SELECT * FROM employee';
-        pool.query(sql, (err: Error, result: QueryResult) => {
-            if (err) {
-              console.error('Error executing query', err.stack);
-              return;
-            }
-            const { rows } = result;
-            console.table(rows);
-          });
-        return this.employees;
-
-    }
+    
+        public getEmployees(): Promise<any[]> {
+            const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) as Manager from employee 
+            LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.id = department.id 
+            LEFT JOIN employee manager on manager.id = employee.manager_id`;
+            return new Promise((resolve, reject) => {
+                pool.query(sql, (err: Error, result: QueryResult) => {
+                    if (err) {
+                        console.error('Error executing query', err.stack);
+                        reject(err);
+                        return;
+                    }
+                    const { rows } = result;
+                    console.table(rows);
+                    resolve(rows);
+                });
+            });
+        }
 }
 
 export default employees;
+
+//SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) as Manager from employee left join role on employee.role_id = role.id left join department on role.id = department.id left join employee manager on manager.id = employee.manager_id
